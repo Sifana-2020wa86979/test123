8d795369bf108ae62b0b414c685a306187a6e89a
SIFANA <2020wa86979@wilp.bits-pilani.ac.in>
2024-04-30 20:16:37 +0530

    new function added

--- file_1.c
+++ file_1.c
@@ -12,8 +12,16 @@ int main() {
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
 }
+<<<<<<<< HEAD
+
+int addfunc(int a, int b)
+{
+	int c = a+b;
+	return c;
+=======	
 //function added in branch1
 int func_add(int a, int b)
 {
 	return a+b;
-}
+>>>>>>>> branch1
+}	
